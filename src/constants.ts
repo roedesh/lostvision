4ce559501cd811d2cdd84e6b11f924073e3eee8f
Ruud Schroën <ruud.schroen@kabisa.nl>
2020-09-12 10:54:15 +0200

    Increased jump velocity and added levels 2 and 3

--- src/constants.ts
+++ src/constants.ts
@@ -12,7 +12,7 @@ export const TILE_SIZE = 16;
 // Movement
 export const ACCELERATION = 0.4;
 export const GRAVITY = 20;
-export const JUMP_VELOCITY = -5;
+export const JUMP_VELOCITY = -5.5;
 export const MAX_SPEED = 2;
 export const PLAYER_WEIGHT = 0.2;
 
